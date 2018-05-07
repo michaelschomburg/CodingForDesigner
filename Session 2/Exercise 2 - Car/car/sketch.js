102d0ec11ae696372ec16508a77707fc7e361863
Michael <schomburg.michael90@gmail.com>
2018-05-07 14:15:15 +0200

    ,

--- Session 2/Exercise 2 - Car/car/sketch.js
+++ Session 2/Exercise 2 - Car/car/sketch.js
@@ -3,24 +3,24 @@ var count = 0;
 
 function setup() {
   createCanvas(600, 600);
- 
+
 }
 
 function draw() {
   background(230);
   noFill();
-  var move = count
-  
-  ellipse(150 + move, 150, 50, 50);
-  ellipse(300 + move, 150, 50, 50);
+
+
+  ellipse(150 + count, 150, 50, 50);
+  ellipse(300 + count, 150, 50, 50);
   fill(16,122,31)
-  rect(200 + move, 90, 50,30);
-  rect(130 + move, 110, 190, 30);
+  rect(200 + count, 90, 50,30);
+  rect(130 + count, 110, 190, 30);
   strokeWeight(6);
-  line(320 + move, 110, 260 + move, 90);
+  line(320 + count, 110, 260 + count, 90);
   strokeWeight(3);
   fill(120);
-  ellipse(225 + move, 75, 30, 30);
-  
+  ellipse(225 + count, 75, 30, 30);
+
   count = count + 1;
 }
